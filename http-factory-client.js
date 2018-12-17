c1056468be2a5429a381a847d07e19b56a0fd84f
Edwin Mak <cintosyntax@gmail.com>
2018-12-17 09:34:05 -0500

    Add HttpFactoryClient /w tests

---
+++ http-factory-client.js
@@ -0,0 +1,66 @@
+import HttpFactoryServer from './http-factory-server'
+
+class HttpFactoryClient {
+
+  constructor(target = document) {
+    this.target = target;
+  }
+
+  get(path, options = {}) {
+    let getEvent = this._constructEvent('GET', path, options);
+    this.target.dispatchEvent(getEvent);
+
+    return getEvent.detail.request;
+  }
+
+  post(path, options = {}) {
+    let postEvent = this._constructEvent('POST', path, options);
+    this.target.dispatchEvent(postEvent);
+
+    return postEvent.detail.request;
+  }
+
+  put(path, options = {}) {
+    let putEvent = this._constructEvent('PUT', path, options);
+    this.target.dispatchEvent(putEvent);
+
+    return putEvent.detail.request;
+  }
+
+  patch(path, options = {}) {
+    let patchEvent = this._constructEvent('PATCH', path, options);
+    this.target.dispatchEvent(patchEvent);
+
+    return patchEvent.detail.request;
+  }
+
+  delete(path, options = {}) {
+    let deleteEvent = this._constructEvent('DELETE', path, options);
+    this.target.dispatchEvent(deleteEvent);
+
+    return deleteEvent.detail.request;
+  }
+
+  _constructEvent(method, path, options) {
+    let requestDetail = Object.assign(
+      {
+        method: method,
+        path: path
+      },
+      options
+    );
+
+    return new CustomEvent(
+      HttpFactoryServer.REQUEST_EVENT_TYPE,
+      {
+        bubbles: true,
+        composed: true,
+        detail: {
+          requestDetail: requestDetail
+        }
+      }
+    )
+  }
+}
+
+export default HttpFactoryClient;
---
+++ http-factory-client.test.js
@@ -0,0 +1,197 @@
+import HttpFactoryClient from './http-factory-client.js'
+import HttpFactoryServer from './http-factory-server'
+
+describe('HttpFactoryClient', () => {
+
+  // Add mock function that mimics HttpFactoryServer
+  // response. Rather than depending on the Server logic this
+  // should be able to run without Server.
+  const mockServerResponse = jest.fn((event) => {
+    event.detail.request = fetch('fake-url', {}).then(res => res.json())
+  })
+  const mockResponseBody = { fakeData: 'i-am-a-fake' };
+
+  beforeEach(() => {
+    fetch.mockResponse(JSON.stringify(mockResponseBody));
+    document.dispatchEvent = mockServerResponse;
+  })
+
+  afterEach(() => {
+    mockServerResponse.mockClear();
+  })
+
+  describe('get', () => {
+    let subject = () => {
+      return client.get(fakePath, { queryParams: queryParams});
+    };
+
+    let fakePath = 'fake-path';
+    let queryParams = { foo: "bar" };
+    let client;
+
+    beforeEach(() => {
+      client = new HttpFactoryClient();
+    })
+
+    test('that it fetches the expected data', () => {
+      return subject().then((data) => {
+        expect(data).toMatchObject(mockResponseBody);
+      })
+    });
+
+    test('that it dispatchs the GET event', () => {
+      subject();
+
+      expect(mockServerResponse).toHaveBeenCalled()
+
+      let capturedEvent = mockServerResponse.mock.calls[0][0];
+
+      expect(capturedEvent.type).toBe(HttpFactoryServer.REQUEST_EVENT_TYPE);
+      expect(capturedEvent.detail.requestDetail).toMatchObject({
+        method: 'GET',
+        path: fakePath,
+        queryParams: queryParams
+      })
+    })
+  })
+
+  describe('post', () => {
+    let subject = () => {
+      return client.post(fakePath, { body: body });
+    };
+
+    let fakePath = 'fake-path';
+    let body = { foo: "bar" };
+
+    let client;
+    beforeEach(() => {
+      client = new HttpFactoryClient();
+    })
+
+    test('that it fetches the expected data', () => {
+      return subject().then((data) => {
+        expect(data).toMatchObject(mockResponseBody);
+      })
+    });
+
+    test('that it dispatchs the POST event', () => {
+      subject();
+
+      expect(mockServerResponse).toHaveBeenCalled()
+
+      let capturedEvent = mockServerResponse.mock.calls[0][0];
+
+      expect(capturedEvent.type).toBe(HttpFactoryServer.REQUEST_EVENT_TYPE);
+      expect(capturedEvent.detail.requestDetail).toMatchObject({
+        method: 'POST',
+        path: fakePath,
+        body: body
+      })
+    })
+  })
+
+  describe('put', () => {
+    let subject = () => {
+      return client.put(fakePath, { body: body });
+    };
+
+    let fakePath = 'fake-path';
+    let body = { foo: "bar" };
+
+    let client;
+    beforeEach(() => {
+      client = new HttpFactoryClient();
+    })
+
+    test('that it fetches the expected data', () => {
+      return subject().then((data) => {
+        expect(data).toMatchObject(mockResponseBody);
+      })
+    });
+
+    test('that it dispatchs the PUT event', () => {
+      subject();
+
+      expect(mockServerResponse).toHaveBeenCalled()
+
+      let capturedEvent = mockServerResponse.mock.calls[0][0];
+
+      expect(capturedEvent.type).toBe(HttpFactoryServer.REQUEST_EVENT_TYPE);
+      expect(capturedEvent.detail.requestDetail).toMatchObject({
+        method: 'PUT',
+        path: fakePath,
+        body: body
+      })
+    })
+  })
+
+  describe('patch', () => {
+    let subject = () => {
+      return client.patch(fakePath, { body: body });
+    };
+
+    let fakePath = 'fake-path';
+    let body = { foo: "bar" };
+
+    let client;
+    beforeEach(() => {
+      client = new HttpFactoryClient();
+    })
+
+    test('that it fetches the expected data', () => {
+      return subject().then((data) => {
+        expect(data).toMatchObject(mockResponseBody);
+      })
+    });
+
+    test('that it dispatchs the PATCH event', () => {
+      subject();
+
+      expect(mockServerResponse).toHaveBeenCalled()
+
+      let capturedEvent = mockServerResponse.mock.calls[0][0];
+
+      expect(capturedEvent.type).toBe(HttpFactoryServer.REQUEST_EVENT_TYPE);
+      expect(capturedEvent.detail.requestDetail).toMatchObject({
+        method: 'PATCH',
+        path: fakePath,
+        body: body
+      })
+    })
+  })
+
+
+  describe('delete', () => {
+    let subject = () => {
+      return client.delete(fakePath);
+    };
+
+    let fakePath = 'fake-path';
+
+    let client;
+    beforeEach(() => {
+      client = new HttpFactoryClient();
+    })
+
+    test('that it fetches the expected data', () => {
+      return subject().then((data) => {
+        expect(data).toMatchObject(mockResponseBody);
+      })
+    });
+
+    test('that it dispatchs the DELETE event', () => {
+      subject();
+
+      expect(mockServerResponse).toHaveBeenCalled()
+
+      let capturedEvent = mockServerResponse.mock.calls[0][0];
+
+      expect(capturedEvent.type).toBe(HttpFactoryServer.REQUEST_EVENT_TYPE);
+      expect(capturedEvent.detail.requestDetail).toMatchObject({
+        method: 'DELETE',
+        path: fakePath
+      })
+    })
+  })
+
+})
